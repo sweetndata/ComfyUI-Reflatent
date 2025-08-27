4f1ad3a9a19f648663ad81d1ff95fe6f033dcf64
sjs <sjs@sweetndata.com>
2025-08-28 01:33:55 +0900

    first commit

---
+++ ComfyUI_Reflatent/__init__.py
@@ -0,0 +1,40 @@
+
+class RefLatent:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {"required": {"conditioning": ("CONDITIONING", ),
+                            },
+                "optional": {"latent": ("LATENT", ),}
+               }
+
+    RETURN_TYPES = ("CONDITIONING",)
+    FUNCTION = "append"
+
+    CATEGORY = "advanced/conditioning/edit_models"
+    DESCRIPTION = "This node sets the guiding latent for an edit model. If the model supports it you can chain multiple to set multiple reference images."
+
+    def append(self, conditioning, latent=None):
+        if latent is not None:
+            conditioning = self.conditioning_set_values(conditioning, {"reference_latents": [latent["samples"]]}, append=True)
+        return (conditioning, )
+
+    def conditioning_set_values(conditioning, values={}, append=False):
+    c = []
+    for t in conditioning:
+        n = [t[0], t[1].copy()]
+        for k in values:
+            val = values[k]
+            if append:
+                old_val = n[1].get(k, None)
+                if old_val is not None:
+                    val = old_val + val
+
+            n[1][k] = val
+        c.append(n)
+
+    return c
+
+
+NODE_CLASS_MAPPINGS = {
+    "RefLatent": RefLatent,
+}
