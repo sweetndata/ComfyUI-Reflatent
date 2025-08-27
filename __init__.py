4fe75adc17274a8304dd4c67941e7e95fa9a37f8
sjs <sjs@sweetndata.com>
2025-08-28 01:36:44 +0900

    change init

--- __init__.py
+++ __init__.py
@@ -2,6 +2,5 @@ import sys
 
 python = sys.executable
 
-from .ComfyUI_Image_Harmonizer import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
-
-__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
+from .ComfyUI_Reflatent import NODE_CLASS_MAPPINGS
+__all__ = ['NODE_CLASS_MAPPINGS']
