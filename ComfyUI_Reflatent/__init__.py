2a6faa16e06d68dde0c500f2e4fe1ec55f5f0a18
sjs <sjs@sweetndata.com>
2025-08-28 01:38:54 +0900

    change __init__.py

--- ComfyUI_Reflatent/__init__.py
+++ ComfyUI_Reflatent/__init__.py
@@ -18,21 +18,21 @@ class RefLatent:
             conditioning = self.conditioning_set_values(conditioning, {"reference_latents": [latent["samples"]]}, append=True)
         return (conditioning, )
 
-    def conditioning_set_values(conditioning, values={}, append=False):
-    c = []
-    for t in conditioning:
-        n = [t[0], t[1].copy()]
-        for k in values:
-            val = values[k]
-            if append:
-                old_val = n[1].get(k, None)
-                if old_val is not None:
-                    val = old_val + val
+    def conditioning_set_values(self, conditioning, values={}, append=False):
+        c = []
+        for t in conditioning:
+            n = [t[0], t[1].copy()]
+            for k in values:
+                val = values[k]
+                if append:
+                    old_val = n[1].get(k, None)
+                    if old_val is not None:
+                        val = old_val + val
 
-            n[1][k] = val
-        c.append(n)
+                n[1][k] = val
+            c.append(n)
 
-    return c
+        return c
 
 
 NODE_CLASS_MAPPINGS = {
